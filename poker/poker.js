26c8dce38f3d4e91765077d58959db4217baf0c8
mobaig23 <m-baig@kellogg.northwestern.edu>
2017-01-31 15:05:06 -0600

    enabled prevent default

--- poker/poker.js
+++ poker/poker.js
@@ -30,7 +30,7 @@ window.getDeck = function() {
 // END DO NOT CHANGE CODE
 
 $(function() {
-  $(".deal").on("click", function(event) {
+  $("a").on("click", function(event) {
   event.preventDefault()
   $("#c1").attr("src", "http://golearntocode.com/images/cards/" + getDeck(2,10) + ".png")
   $("#c2").attr("src", "http://golearntocode.com/images/cards/" + getDeck(2,10) + ".png")
